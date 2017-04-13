d525dc4621ca984e99ba4e16217abbf637fbf202
Mathspy <mathspy257@gmail.com>
2017-04-13 19:26:19 +0200

    Three boxes appear in the wild

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello Life!");
+var img;
+function preload() {
+  img = loadImage("drawable/box.png");
+}
+
+function setup() {
+	createCanvas(600, 480);
+	background(51);
+	imageMode(CENTER)
+	image(img, width / 2, height / 2, 100, 100);
+	image(img, 3*width / 4, height / 2, 100, 100);
+	image(img, width / 4, height / 2, 100, 100);
+}
